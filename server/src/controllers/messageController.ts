d5a6fda5b789e19f7675545ebfd8016d78ee04a5
Mark Wallace <mark@leker.to>
2024-11-20 00:26:37 -0800

    Added typescript support, moved messageController to .ts

--- server/src/controllers/messageController.ts
+++ server/src/controllers/messageController.ts
@@ -1,15 +1,19 @@
-// src/controllers/messageController.js
+// src/controllers/messageController.ts
+import { Request, Response } from 'express';
 import messageService from '../services/messageService.js';
 
-export function postReply(req, res) {
+export function postReply(req: Request, res: Response): void {
   console.log('Received body:', req.body);
   const message = req.body;
 
   if (!message) {
-    return res.status(400).json({ error: 'Message is required' });
+    res.status(400).json({ error: 'Message is required' });
+    return;
   }
+
   console.log('Received message:', message);
   message.publicKey = req.publicKey;
+
   messageService.broadcastMessage(message);
   res.status(200).json({ status: 'OK' });
 }
